2ad405ccb43ba57635f3857e0f094ad333dc7835
Anisuzzaman Khan <aniskhan001@outlook.com>
2018-11-03 03:09:40 +0600

    satisfying test job

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const PORT = 3000;
 
 app.use(bodyParser.json());
 
+// Router definitions
+app.get('/', (req, res) => {
+    res.status(200).send("NAPI NAPI")
+})
 app.use('/users', require('./api/router'));
 
 app.listen(PORT);
 console.log('API working on PORT: ' + PORT);
+
+module.exports = app

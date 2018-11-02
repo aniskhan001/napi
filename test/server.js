89cd9112303a40c1f2823c7302ec8616f3b2fce4
Anisuzzaman Khan <aniskhan001@outlook.com>
2018-11-03 03:08:38 +0600

    added minimal tests

---
+++ test/server.js
@@ -0,0 +1,23 @@
+const chai = require("chai")
+const chaiHttp = require('chai-http')
+const app = require("../server")
+chai.should()
+chai.use(chaiHttp)
+
+describe('HEALTH check!', () => {
+    it('should return 200 on index', (done) => {
+        chai.request(app).get('/').end( (err, res) => {
+            res.should.have.status(200)
+            done()
+        })
+    })
+
+    it('should return 404 on bad endpoints', (done) => {
+        chai.request(app).get('/dang').end( (err, res) => {
+            res.should.have.status(404)
+            done()
+        })
+    })
+})
+
+module.exports = app
